f14471265524b28f2fe12e87dfd89bf8943f440d
lipovskaia <natalia.lipovskaia@gmail.com>
2018-03-01 21:42:51 -0800

    arrays

---
+++ js/01-arrays-practice.js
@@ -0,0 +1,115 @@
+/*  //STEP 1
+var movies  =  ["Black panter", "Titanic", "Scar Face", "Paddington", "Paddington2"]
+window.document.write(movies[1] + "<br>");
+
+//STEP 2
+var movies = new Array(5);
+movies[0] = "Black panter";
+movies[1] = "Titanic";
+movies[2] = "Scar Face";
+movies[3] = "Paddington";
+movies[4] = "Paddington 2";
+
+window.document.write(movies[0] + "<br>"); 
+ 
+
+//STEP 3
+var movies = new Array(5);
+movies[0] = "Black panter";
+movies[1] = "Titanic";
+movies[2] = "Scar Face";
+movies[3] = "Paddington";
+movies[4] = "Paddington 2";
+movies[5] = "Paddington 3";
+ window.document.write(movies.length + "<br>"); */
+
+/*    //STEP 4
+var movies = [];
+movies[0] = "Black panter";
+movies[1] = "Titanic";
+movies[2] = "Scar Face";
+movies[3] = "Paddington";
+movies[4] = "Paddington 2";
+delete movies[0];
+window.document.write(movies); */
+//STEP 5
+/* var i;
+var movies = [];
+movies[0] = "Black panter";
+movies[1] = "Titanic";
+movies[2] = "Scar Face";
+movies[3] = "Paddington";
+movies[4] = "Paddington 2";
+movies[5] = "Paddington 3";
+movies[6] = "Paddington 4";
+for (i = 0; i < movies.length; i+= 1 ){
+    window.document.write(movies[i] + "<br>");
+}
+
+//STEP 6
+/*  var index;
+var movies = [];
+movies[0] = "Black panter";
+movies[1] = "Titanic";
+movies[2] = "Scar Face";
+movies[3] = "Paddington";
+movies[4] = "Paddington 2";
+movies[5] = "Paddington 3";
+movies[6] = "Paddington 4";
+for (index in movies){
+    window.document.write(movies[index] + "<br>");
+}
+ 
+
+//STEP 7
+ var index;
+var movies = [];
+movies[0] = "Black panter";
+movies[1] = "Titanic";
+movies[2] = "Scar Face";
+movies[3] = "Paddington";
+movies[4] = "Paddington 2";
+movies[5] = "Paddington 3";
+movies[6] = "Paddington 4";
+for (index in movies){
+    window.document.write(movies.sort()[index] + "<br>");
+}
+*/
+//STEP 8
+var i;
+var movies = [];
+movies[0] = "Black panter";
+movies[1] = "Titanic";
+movies[2] = "Scar Face";
+movies[3] = "Paddington";
+movies[4] = "Paddington 2";
+movies[5] = "Paddington 3";
+movies[6] = "Paddington 4";
+for (i = 0; i < movies.length; i+= 1 ){
+    window.document.write(movies[i] + "<br>");
+}
+var leastFavMovies = []
+
+//STEP 9
+
+//STEP 10
+
+//STEP 11
+
+//STEP 12
+
+//STEP 13
+
+//STEP 14
+
+//STEP 15
+
+//STEP 16
+
+//STEP 17
+
+//STEP 18
+
+//STEP 19
+
+//STEP 20
