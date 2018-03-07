2e4ec16abfaa5314ea87f8d1aaa597098a89249d
lipovskaia <natalia.lipovskaia@gmail.com>
2018-03-06 21:41:29 -0800

    new

--- js/01-arrays-practice.js
+++ js/01-arrays-practice.js
@@ -1,3 +1,4 @@
+/*eslint-env browser */
 /*  //STEP 1
 var movies  =  ["Black panter", "Titanic", "Scar Face", "Paddington", "Paddington2"]
 window.document.write(movies[1] + "<br>");
@@ -76,7 +77,52 @@ for (index in movies){
 }
 */
 //STEP 8
-var i;
+/*
+var movies = [], i;
+movies[0] = "Black panter";
+movies[1] = "Titanic";
+movies[2] = "Scar Face";
+movies[3] = "Paddington";
+movies[4] = "Paddington 2";
+movies[5] = "Paddington 3";
+movies[6] = "Paddington 4";
+window.document.write("Movies I like: " + "<br>");
+window.document.write("<br>");
+
+for (i = 0; i < 7; i++ ){
+    window.document.write( movies[i] + "<br>");
+}
+window.document.write("<br>");
+
+var leastFavMovies = ["Batman", "Harry Potter", "Top Gun"] ;
+window.document.write("Movies I don't like: " + "<br>");
+window.document.write("<br>");
+for (i = 0; i < 3; i++ ){
+    
+    window.document.write(leastFavMovies[i] + "<br>");
+}
+
+*/
+ 
+
+//STEP 9
+/*
+var movies = [], i;
+movies[0] = "Black panter";
+movies[1] = "Titanic";
+movies[2] = "Scar Face";
+movies[3] = "Paddington";
+movies[4] = "Paddington 2";
+movies[5] = "Paddington 3";
+movies[6] = "Paddington 4";
+
+var leastFavMovies = ["Batman", "Harry Potter", "Top Gun"] ;
+movies = movies.concat(leastFavMovies);
+for( i=0; i< 10; i++){
+window.document.write(movies.sort().reverse()[i] + "<br>");
+} */
+//STEP 10
+/*
 var movies = [];
 movies[0] = "Black panter";
 movies[1] = "Titanic";
@@ -85,22 +131,67 @@ movies[3] = "Paddington";
 movies[4] = "Paddington 2";
 movies[5] = "Paddington 3";
 movies[6] = "Paddington 4";
-for (i = 0; i < movies.length; i+= 1 ){
-    window.document.write(movies[i] + "<br>");
-}
-var leastFavMovies = []
 
-//STEP 9
+var leastFavMovies = ["Batman", "Harry Potter", "Top Gun"] ;
+movies = movies.concat(leastFavMovies);
 
-//STEP 10
+window.document.write(movies.sort().reverse().pop() );
+*/
 
 //STEP 11
+/*
+var movies = [];
+movies[0] = "Black panter";
+movies[1] = "Titanic";
+movies[2] = "Scar Face";
+movies[3] = "Paddington";
+movies[4] = "Paddington 2";
+movies[5] = "Paddington 3";
+movies[6] = "Paddington 4";
+
+var leastFavMovies = ["Batman", "Harry Potter", "Top Gun"] ;
+movies = movies.concat(leastFavMovies);
+
+window.document.write(movies.sort().reverse().shift() );
+*/
+
 
 //STEP 12
 
-//STEP 13
 
+//STEP 13
+//employeeid, name, title, department, and whether or not the employee is a current employee
+var employee1 = [];
+employee1["id"] = 1;
+employee1["name"] = "John";
+employee1["title"] = "Manager";
+employee1["department"] = "Entertainment";
+employee1["isCurrent"] = true;
+var employee2 = [];
+employee2["id"] = 1;
+employee2["name"] = "Mark";
+employee2["title"] = "Cashier";
+employee2["department"] = "Sales";
+employee2["isCurrent"] = false;
+ 
+var employees = [];
+employees.push(employee1);
+employees.push(employee2);
+
+window.document.write(employees["name"]);
+/* 
+
+*/
 //STEP 14
+/* var i;
+var employee1 = [ 1, "John", "Manager", "Entertainment", "Active"];
+var employee2 = [ 2, "Michael", "Cashier", "Sales", "Inactive"];
+var employees; 
+employees =  employee1.concat( employee2);
+for (i = 0; i < employees.length ; i += 1 ) {
+  window.document.write(employees[i] [1] + " " + employees[i] [6] );
+  
+} */ 
 
 //STEP 15
 
